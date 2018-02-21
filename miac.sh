17b81aa4428088af0f1f614480636de5b3dab214
Tom Bridge <tom@technolutionary.com>
2018-02-21 15:51:46 +0000

    Commit Zero. Stated Goals and that's all.

---
+++ miac.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+## This script is designed to setup a Munki repository on a local machine for sync to the cloud.
+
+## Goals:
+## This script should:
+## Download the Munki Tools
+## Install Them
+## Build a local repository
+## Install autopkg 
+## Download Things and put them into the Repo
+## Install Repo Tools
+
+exit
